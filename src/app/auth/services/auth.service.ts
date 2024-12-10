184d9b3e83cf2b2aa2af7c1ce414ebdf205ded10
kenier <kenier202@gmail.com>
2024-12-10 14:44:01 -0500

    feat(AuthService): implement login method

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ServicesService {
+export class AuthService {
+
+  private readonly baseUrl : string = environment.baseUrl;
+  private http : HttpClient = inject(HttpClient);
+
+  private _currentUser = signal<User|null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
+  public currentUser = computed( () => this._currentUser );
+  public authStatus = computed( () => this._authStatus );
+
 
   constructor() { }
+
+  login ( email: string, password: string ) : Observable<boolean> {
+    const url = `${this.baseUrl}/auth/login`;
+    const body = {email, password};
+
+    return this.http.post<LoginResponse>(url,body)
+    .pipe(
+      tap(({user, token}) => {
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.autheticated);
+        localStorage.setItem('token',token);
+        console.log(user,token);
+      }),
+
+      map( () => true),
+
+      catchError( err => throwError( () => err.error.message) )
+    );
+  }
 }
