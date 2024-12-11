169b56919ee44ff347829a460f8c9434640d1b46
kenier <kenier202@gmail.com>
2024-12-11 08:34:47 -0500

    feat(AuthService): add checkAuthToken method

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +13,20 @@ export class AuthService {
   private http : HttpClient = inject(HttpClient);
 
   private _currentUser = signal<User|null>(null);
-  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+  private _authStatus =  signal<AuthStatus>(AuthStatus.checking);
 
-  public currentUser = computed( () => this._currentUser );
-  public authStatus = computed( () => this._authStatus );
+  public currentUser = computed( () => this._currentUser() );
+  public authStatus =  computed( () => this._authStatus() );
 
 
   constructor() { }
+
+  private setAuthenticated(user: User, token: string): boolean {
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.autheticated);
+    localStorage.setItem('token',token);
+    return true;
+  }
 
   login ( email: string, password: string ) : Observable<boolean> {
     const url = `${this.baseUrl}/auth/login`;
@@ -27,14 +34,25 @@ export class AuthService {
 
     return this.http.post<LoginResponse>(url,body)
     .pipe(
-      tap(({user, token}) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.autheticated);
-        localStorage.setItem('token',token);
-        console.log(user,token);
-      }),
+      map(
+        ({user, token}) => this.setAuthenticated(user,token) ),
 
-      map( () => true),
+      catchError( err => throwError( () => err.error.message) )
+    );
+  }
+
+  checkAuthToken(): Observable<boolean> {
+    const url = `${this.baseUrl}/auth/check-token`;
+    const token = localStorage.getItem('token');
+
+    if (!token) return of(false);
+
+    const headers = new HttpHeaders()
+    .set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<CheckTokenResponse>(url,{headers}) .pipe(
+      map(
+        ({user, token}) => this.setAuthenticated(user,token) ),
 
       catchError( err => throwError( () => err.error.message) )
     );
